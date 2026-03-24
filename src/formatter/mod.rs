73a96ae66fd7dc96aa407f6c4b35cc47a33134c6
dylantf <dylantf@gmail.com>
2026-03-24 19:57:10 +0100

    Add initial AST-to-document conversion for formatter

--- src/formatter/mod.rs
+++ src/formatter/mod.rs
@@ -1,3 +1,13 @@
 mod doc;
+mod format;
 
 pub use doc::{Doc, pretty};
+pub use format::format_program;
+
+use crate::ast::Program;
+
+/// Format a parsed program to a string with the given line width.
+pub fn format(program: &Program, width: usize) -> String {
+    let doc = format_program(program);
+    pretty(width, &doc)
+}
