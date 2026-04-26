28aa3defffe7d46d8f941351bd37d22a04f49c70
dylantf <dylantf@gmail.com>
2026-04-26 13:23:00 +0200

    Add int<->hex parsing

--- src/stdlib/Int.bridge.erl
+++ src/stdlib/Int.bridge.erl
@@ -1,5 +1,5 @@
 -module(std_int_bridge).
--export([parse/1, shift_left/2, shift_right/2]).
+-export([parse/1, shift_left/2, shift_right/2, to_hex/1, parse_hex/1]).
 
 parse(S) ->
     case string:to_integer(S) of
@@ -13,3 +13,19 @@ shift_left(Bits, N) ->
 
 shift_right(Bits, N) ->
     N bsr Bits.
+
+to_hex(N) when N < 0 ->
+    <<"-", (string:lowercase(integer_to_binary(-N, 16)))/binary>>;
+to_hex(N) ->
+    string:lowercase(integer_to_binary(N, 16)).
+
+parse_hex(<<"-", Rest/binary>>) ->
+    case parse_hex(Rest) of
+        {just, N} -> {just, -N};
+        Nothing -> Nothing
+    end;
+parse_hex(S) when is_binary(S), byte_size(S) > 0 ->
+    try {just, binary_to_integer(S, 16)}
+    catch _:_ -> {nothing}
+    end;
+parse_hex(_) -> {nothing}.
