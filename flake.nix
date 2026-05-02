8f0e27cbde03a4d22608c03b87951dbeb88e0b3c
dylantf <dylantf@gmail.com>
2026-05-02 17:25:23 +0200

    Update flake outputs

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Saga dev shell";
+  description = "Saga: a functional language with algebraic effects, compiling to BEAM";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
@@ -12,8 +12,54 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+
+        cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+
+        runtimeDeps = [
+          pkgs.erlang
+          pkgs.rebar3
+        ];
+
+        saga = pkgs.rustPlatform.buildRustPackage {
+          pname = "saga";
+          version = cargoToml.package.version;
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+
+          doCheck = false;
+
+          nativeBuildInputs = [
+            pkgs.pkg-config
+            pkgs.makeBinaryWrapper
+          ];
+          buildInputs = [
+            pkgs.openssl
+          ];
+
+          postInstall = ''
+            wrapProgram $out/bin/saga \
+              --prefix PATH : ${pkgs.lib.makeBinPath runtimeDeps}
+          '';
+
+          meta = with pkgs.lib; {
+            description = "Functional language with algebraic effects, compiling to BEAM";
+            homepage = "https://github.com/dylantf/saga";
+            license = licenses.gpl3Only;
+            mainProgram = "saga";
+          };
+        };
       in
       {
+        packages = {
+          default = saga;
+          saga = saga;
+        };
+
+        apps.default = {
+          type = "app";
+          program = "${saga}/bin/saga";
+        };
+
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = [
             pkgs.rustc
@@ -23,15 +69,16 @@
             pkgs.rust-analyzer
             pkgs.rustfmt
             pkgs.clippy
-          ];
+          ]
+          ++ runtimeDeps;
 
           buildInputs = [
+            pkgs.bashInteractive
             pkgs.openssl
           ];
 
           RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
         };
-
       }
     );
 }
